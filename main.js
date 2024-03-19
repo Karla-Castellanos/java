d0ca70252ec6cb9fe3d3e7eddbcf18bfeb8b2dc3
karla-castellanos <karlafernanda.castellanosgarcia@gmail.com>
2024-03-19 21:59:27 +0100

    update java

---
+++ main.js
@@ -0,0 +1,14 @@
+function botonparaocultar(id) {
+    
+let botonparaocultartexto=document.getElementById(id)
+
+    if (botonparaocultartexto.style.display =="none" ) {
+        botonparaocultartexto.style.display ="flex"
+    } else {
+        botonparaocultartexto.style.display="none"
+        
+    }
+
+}
+let nombre="karla"
+console.log(nombre);
